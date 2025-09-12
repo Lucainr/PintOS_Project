340690aa105e796633e6ebd49dd59df05701179d
leeseungjun <cjstkrak@gmail.com>
2025-09-13 00:31:08 +0900

    add: 프로세스유지용 while작성

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -21,7 +21,7 @@ enum thread_status
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
 typedef int tid_t;
-#define TID_ERROR ((tid_t)-1) /* Error value for tid_t. */
+#define TID_ERROR ((tid_t) - 1) /* Error value for tid_t. */
 
 /* Thread priorities. */
 #define PRI_MIN 0      /* Lowest priority. */
@@ -112,6 +112,7 @@ struct thread
     int niceness;
     int recent_cpu;
     struct list_elem all_elem;
+    // uint64_t *pml4;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
