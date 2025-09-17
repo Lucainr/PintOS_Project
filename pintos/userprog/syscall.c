10922073a40d4fdd5a9408f1d7550d708cd8b6c6
leeseungjun <cjstkrak@gmail.com>
2025-09-17 13:46:43 +0900

    add: create 시스템콜, 사용자 영역검증함수 추가

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -1,18 +1,23 @@
 #include "userprog/syscall.h"
+#include "filesys/filesys.h"
 #include "intrinsic.h"
 #include "threads/flags.h"
 #include "threads/init.h" // power_off
 #include "threads/interrupt.h"
 #include "threads/loader.h"
+#include "threads/synch.h" // lock
 #include "threads/thread.h"
 #include "userprog/gdt.h"
 #include <lib/kernel/stdio.h>
 #include <stdio.h>
+#include <string.h>
+
 #include <syscall-nr.h>
 #include <userprog/process.h>
 
 void syscall_entry(void);
 void syscall_handler(struct intr_frame *);
+bool create(const char *name, off_t initial_size);
 
 /* System call.
  *
@@ -23,9 +28,12 @@ void syscall_handler(struct intr_frame *);
  * The syscall instruction works by reading the values from the the Model
  * Specific Register (MSR). For the details, see the manual. */
 
+#define FILE_NAME_MAX 14
 #define MSR_STAR 0xc0000081         /* Segment selector msr */
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
+static struct lock filesyslock;
+static void check_address(const void *addr);
 
 void syscall_init(void)
 {
@@ -38,6 +46,8 @@ void syscall_init(void)
      * mode stack. Therefore, we masked the FLAG_FL. */
     write_msr(MSR_SYSCALL_MASK,
               FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+
+    lock_init(&filesyslock); // lock 초기화
 }
 
 /* The main system call interface */
@@ -54,6 +64,18 @@ void syscall_handler(struct intr_frame *f UNUSED)
         power_off();
         break;
     }
+    case SYS_CREATE:
+    { // rdi file, rsi size
+
+        f->R.rax = create(f->R.rdi, f->R.rsi);
+        // 모든 반환형이 있는 시스템콜은 rax에 채워줘야한다.
+        break;
+    }
+
+        // bool create(const char *file, unsigned initial_size)
+        // {
+        //     return syscall2(SYS_CREATE, file, initial_size);
+        // }
     case SYS_WRITE:
     {
         if (f->R.rdi == 1)
@@ -84,4 +106,31 @@ void exit(int status)
     sema_up(&cur->wait_sema);
     // 현재 스레드 종료
     thread_exit();
+}
+
+bool create(const char *name, off_t initial_size)
+{
+    check_address(name);
+    bool result; // 결과값 저장
+    if (name == NULL)
+        return false;
+    if (strlen(name) < 1 || strlen(name) > FILE_NAME_MAX)
+        return false;
+
+    lock_acquire(&filesyslock);
+    result = filesys_create(name, initial_size);
+    lock_release(&filesyslock);
+
+    return result;
+}
+
+static void check_address(const void *addr)
+{
+    // 포인터가 아예들어오지 않았던가 커널영역으로 주소를 보냈을때 강제종료
+    if (addr == NULL || !is_user_vaddr(addr) ||
+        pml4_get_page(thread_current()->pml4, addr) == NULL)
+    {
+        exit(-1); // 프로세스 강제 종료
+        // 이거 공부
+    }
 }
