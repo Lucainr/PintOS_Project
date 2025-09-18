6663b14af517770c1e162a3a82b80c1ffbe0f475
leeseungjun <cjstkrak@gmail.com>
2025-09-18 16:05:22 +0900

    add: fork 매개변수 구조체 생성

--- pintos/include/userprog/process.h
+++ pintos/include/userprog/process.h
@@ -10,5 +10,12 @@ int process_wait(tid_t);
 void process_exit(void);
 void process_activate(struct thread *next);
 struct thread *find_child(struct list *list, tid_t tid);
-
+/* fork 전용 구조체 */
+struct fork_aux
+{
+    struct intr_frame *p_if;
+    struct thread *parent;
+    struct semaphore done;
+    bool success;
+};
 #endif /* userprog/process.h */
